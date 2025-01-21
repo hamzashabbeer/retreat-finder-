fc842201760bbd6bf4632df8310e6704ef12d6c7
hamzashabbeer <hamzashabbir2k@gmail.com>
2025-01-21 21:43:03 +0500

    Fix: Add path aliases and update TypeScript config

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import SearchBar from '../components/forms/SearchBar';
-import RetreatCard from '../components/common/RetreatCard';
-import type { Retreat } from '../types';
+import SearchBar from '@components/forms/SearchBar';
+import RetreatCard from '@components/common/RetreatCard';
+import type { Retreat } from '@types';
 
 const HomePage: React.FC = () => {
   // Mock data for featured retreats
